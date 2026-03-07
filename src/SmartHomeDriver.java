18ab01ff054e6bafa125c52e84b06abbcf551827
ozdenhuseyin <ohus0003@student.monash.edu>
2026-03-08 00:51:33 +1100

    Just finished but I hate git

--- src/SmartHomeDriver.java
+++ src/SmartHomeDriver.java
@@ -9,11 +9,20 @@ public class SmartHomeDriver {
         bulb1.turnOn();
         bulb1.setColour("red");
 
-        SmartBulb blub2 = new SmartBulb(false, "Yellow");
+        SmartBulb bulb2 = new SmartBulb(false, "Yellow");
 
 
+        SmartBulb bulbArray[]= new SmartBulb[2];
+        bulbArray[0] = bulb1;
+        bulbArray[1] = bulb2;
 
+        //dispolay bulb data
+        System.out.println("BUlb 1 is on: " + bulbArray[0].isStatus());
+        System.out.println("BUlb 2 is on: " + bulbArray[1].isStatus());
 
+        for (int i = 0; i < bulbArray.length ; i++){
+            System.out.println("BUlb " + (i+1) +" is on: " + bulbArray[i].isStatus());
+        }
     }
 
 }
